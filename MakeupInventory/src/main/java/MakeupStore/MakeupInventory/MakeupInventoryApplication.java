7b7174b573b5b7522c5190a3f0b8b65e71568d6e
Nikki1614 <163075510+Nikki1614@users.noreply.github.com>
2025-05-05 11:39:09 -0500

    Add files via upload

---
+++ MakeupInventory/src/main/java/MakeupStore/MakeupInventory/MakeupInventoryApplication.java
@@ -0,0 +1,13 @@
+package MakeupStore.MakeupInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupInventoryApplication.class, args);
+	}
+
+}
---
+++ MakeupInventory/src/test/java/MakeupStore/MakeupInventory/MakeupInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package MakeupStore.MakeupInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
