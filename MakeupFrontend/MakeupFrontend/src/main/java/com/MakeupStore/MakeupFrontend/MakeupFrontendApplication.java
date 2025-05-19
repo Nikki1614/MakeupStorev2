cf54ccc7c72b11ba94b0fa1768c89e980f294d1b
Nikki1614 <163075510+Nikki1614@users.noreply.github.com>
2025-05-19 12:09:19 -0500

    Add files via upload

---
+++ MakeupFrontend/MakeupFrontend/src/main/java/com/MakeupStore/MakeupFrontend/MakeupFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.MakeupStore.MakeupFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupFrontendApplication.class, args);
+	}
+
+}
